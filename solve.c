f554784843767d703d32885bebbfeb9828a79708
alan <alan0988469026@gmail.com>
2020-04-13 14:27:39 +0800

    init Symbol XYZ

--- solve.c
+++ solve.c
@@ -14,6 +14,17 @@ int main(void)
 
     while (1)
     {
+        // init XYZ
+        for (int i = 0; i < 3; i++)
+        {
+            char *c;
+            c[0] = 'x' + i;
+            c[1] = '\0';
+            strcpy(sbtable[sbcount].name, c);
+            sbtable[sbcount].val = 0;
+            sbcount++;
+        }
+
         statement();
     }
     return 0;
